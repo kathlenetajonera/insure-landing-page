a028b65f5dddd3f08e263150685a7ec08251dc62
ktj13 <76245226+ktj13@users.noreply.github.com>
2021-01-07 11:53:50 +0800

    Added scrollTop button and function

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const mobileToggle = document.querySelector(".toggle-btn");
 const mobileMenu = document.querySelector(".menu");
 const overlay = document.querySelector(".overlay");
 
+const scrollTopBtn = document.querySelector("#scroll_top")
+
 mobileToggle.addEventListener("click", () => {
     mobileMenu.classList.toggle("mobile_nav");
     
@@ -18,5 +20,22 @@ mobileToggle.addEventListener("click", () => {
 window.addEventListener("resize", () => {
     if (window.innerWidth > 768) {
         mobileMenu.classList.remove("mobile_nav");
+        mobileToggle.classList.remove("close_icon");
+        document.body.style.overflowY = "scroll";
     }
 })
+
+//shows the scroll top button onscroll
+window.addEventListener("scroll", () => {
+    if (pageYOffset > 600) {
+        scrollTopBtn.classList.add("show");
+
+        scrollTopBtn.addEventListener("click", scrollTop);
+    } else {
+        scrollTopBtn.classList.remove("show");
+    }
+})
+
+const scrollTop = () => {
+    document.documentElement.scrollTop = 0;
+}
